fcd4783c180ae7de1f8853fdc1696475791164d1
teddyzxcv <48234309+teddyzxcv@users.noreply.github.com>
2021-05-29 14:24:45 +0300

    End of project

---
+++ DotnetApp/DatabaseCreate/WebApplication/WebApplication/Controllers/CountryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class CountryController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
